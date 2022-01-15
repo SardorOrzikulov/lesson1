93af1ea7ae73ed7c82744fedd47d2fb8421df20c
Orzikulov Sardor <sardororzikulov55@gmail.com>
2022-01-15 23:58:07 +0500

    information about this sign "\"

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+#rint('hello world')
+#rint('ikkining kvadrati', 2**2, 'ga teng')
+#print('ikkining kvadrati', 2**2,'ga teng')
+#print('"NEXIA" , "TIKO" , "DAMAS" \nko\'rganlar qilar havas' )
+#print('5 ning 4 chi darajasi' , 5**4 , 'ga teng')
+#KEYWORD= '"HELLO WORLD"'
+#print(KEYWORD)
+#print("changed")
+#print("changed2")
+#_='55'
+#yangilik='new'
+#print(yangilik+' '+_)
+#birthYear = 1999
+#name='Abduhalim'
+#surName='Orzikulov'
+#print(f"Men {name} {surName} o`ktam o`g`li {birthYear} yilda tug`ilganman!")
+#print('Hello World')
+#print('Hello\tWorld')
+
--- lesson1.py.py
+++
@@ -1,9 +0,0 @@
-#rint('hello world')
-#rint('ikkining kvadrati', 2**2, 'ga teng')
-#print('ikkining kvadrati', 2**2,'ga teng')
-#print('"NEXIA" , "TIKO" , "DAMAS" \nko\'rganlar qilar havas' )
-#print('5 ning 4 chi darajasi' , 5**4 , 'ga teng')
-KEYWORD= '"HELLO WORLD"'
-print(KEYWORD)
-print("changed")
-print("changed2)
