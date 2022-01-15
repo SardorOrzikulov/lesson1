b75de5d7a210ca7474ed00ac3664f8c299f736da
Orzikulov Sardor <sardororzikulov55@gmail.com>
2022-01-15 19:15:29 +0500

    Initial commit

---
+++ lesson1.py.py
@@ -0,0 +1,8 @@
+#rint('hello world')
+#rint('ikkining kvadrati', 2**2, 'ga teng')
+#print('ikkining kvadrati', 2**2,'ga teng')
+#print('"NEXIA" , "TIKO" , "DAMAS" \nko\'rganlar qilar havas' )
+#print('5 ning 4 chi darajasi' , 5**4 , 'ga teng')
+KEYWORD= '"HELLO WORLD"'
+print(KEYWORD)
+print("changed")
